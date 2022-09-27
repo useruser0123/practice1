d9aec0378c31fab3d884591e251fceae0a9adadc
fujiwara <k.fujiwara.sys22@morijyobi.ac.jp>
2022-09-27 11:05:43 +0900

    Hello World
    100回出力に変更

--- Pra2.java
+++ Pra2.java
@@ -1,7 +1,8 @@
 public class Pra2{
     public static void main(String[] args) {
-        
+        for(int a=0;a<100;a++){
             System.out.println("Hello World");
+        }
         
     }
 }
